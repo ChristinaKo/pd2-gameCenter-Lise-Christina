13f74605a3298196937a3f49e16f32b470688bfb
Lise <koprty@gmail.com>
2014-06-03 12:03:50 -0400

    Gui updated.

--- GUI.java
+++ GUI.java
@@ -19,9 +19,12 @@ public class GUI extends JFrame{
     // protected JTextArea notifications;
     //protected JTextArea plz, instructions;
 
-    public class MyPanel extends JPanel{
+    public class MyPanelGradient extends JPanel{
 	public void paintChildren(Graphics g){
-	    
+	    if (!isOpaque()){
+		super.paintComponent(g);
+		return;
+	    }
 	}
     }
 
@@ -29,6 +32,7 @@ public class GUI extends JFrame{
     protected JMenuBar title;
     protected JLabel label;
     public GUI(){
+	//initial state -> title page
 	frame = new JFrame("Game Center");
 	frame.setSize(400,400);
 	frame.setLocationRelativeTo(null);
@@ -36,12 +40,12 @@ public class GUI extends JFrame{
 
 	title= new JMenuBar();
 	title.setOpaque(true);
-	title.setBackground(new Color(123,143,023));
+	title.setBackground(Color.cyan);
 	title.setPreferredSize(new Dimension(400,20));
 
 	label = new JLabel();
 	label.setOpaque(true);
-	label.setBackground(new Color(248,232,123));
+	label.setBackground(new Color(0,2,123));
 	label.setPreferredSize(new Dimension(400,380));
 
 
@@ -51,15 +55,29 @@ public class GUI extends JFrame{
 	frame.setJMenuBar(title);
 	frame.getContentPane().add(label,BorderLayout.CENTER);
 
-
-
 	frame.pack();
 	frame.setVisible(true);
     }
+    
+    public void mainscreen(){
+
+	ImageIcon buttonimg1 = createImageIcon("images/right.gif");
+	ImageIcon buttonimg2 = createImageIcon("images/middle.gif");
+	ImageIcon buttonimg3 = createImageIcon("images/left.gif");
+
+	b1 = new JButton("Game 1", buttonimg1);
+	b2 = new JButton("Game 2", buttonimg2);
+	b3 = new JButton("Game 3", buttonimg3);
+
+    }
+)
+
 
     public static void main(String[]args){
 	GUI x = new GUI();
-
+	
+	/*add algorithms above as GUI methods
+	  menu options above.
 	
 
 

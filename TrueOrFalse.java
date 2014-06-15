7c876aba1e88e3f013435a73b56983fd1a6611f2
Christina Ko <christinako822@gmail.com>
2014-06-15 17:28:24 -0400

    finished True or False

--- TrueOrFalse.java
+++ TrueOrFalse.java
@@ -4,24 +4,54 @@ import java.util.*;
 public class TrueOrFalse{
     String name;
     int score;
-    String ans;
+    String qans;
+    String quest;
     ArrayList <String> questions = new ArrayList<String>();
     public TrueOrFalse(String n){
 	score = 0;
 	name = n;
     }
-    public String question(){
-	Scanner sc = new Scanner(new File("test.csv"));
-	while(sc.hasNext()){
-	    ArrayList.add(sc.next());
+    public void  setQuestions(){
+	try{
+	    Scanner sc = new Scanner(new File("test.csv"));
+	    while(sc.hasNext()){
+		questions.add(sc.next());
+	    }
 	}
-	
-	return    
-	
+	catch(FileNotFoundException e){
+	}
+    }
+    public String askQuestion(){
+	Random r = new Random();
+	int queNum = r.nextInt();
+	String[] parts = questions.get(queNum).split(",");
+	quest = parts[0];
+	qans = parts[1];
+	return quest;
     }
     public boolean checkAnswer(String ans){
-	if(ans.equals(qans)){
-	    //score  = score + 20;
+	if(ans.toUpperCase().equals(qans.toUpperCase())){
 	    return true;
 	}
+	return false;
+    }
+    public void play(){
+	if(score == 100){
+	    System.out.println("COngratulations");
+	    System.exit(0);
+	}
+	else{
+	    System.out.println(askQuestion());
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("True or False");
+	    String ans = sc.nextLine();
+	    if(checkAnswer(ans)){
+		score = score +10;
+		System.out.println("Correct");
+	    }
+	    else{
+		System.out.println("Incorrect");
+	    }
+	}
+    }    
 }
